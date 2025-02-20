b23e44c79852e1b89bc2bfff4e886cf21ddbdd64
RelexSun <88622289+RelexSun@users.noreply.github.com>
2025-02-20 17:13:33 +0700

    batman

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["DM Sans", "sans-serif"], // Overrides Tailwind's default sans-serif
+      },
+    },
+  },
+  plugins: [],
+};
